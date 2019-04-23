4afd365ee01f08b1a74b017a9b98e99472fd2a8e
Richard F. Alvarez <ralvarez@kth.se>
2019-04-23 17:33:48 +0200

    startNewSale operation, incomplete

--- retailstore/controller/Controller.java
+++ retailstore/controller/Controller.java
@@ -1,5 +1,28 @@
 package retailstore.controller;
 
-public class Controller {
+import retailstore.model.Sale;
+import retailstore.integration.ItemRegistry;
 
+public class Controller {
+	
+	
+	/**
+	 * Creates a new instance.
+	 * 
+	 * @param itemRegistry	
+	 */
+
+	public Controller (ItemRegistry itemRegistry) {
+		
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public void startNewSale() {
+		
+		Sale sale = new Sale();
+	}
+	
 }
--- retailstore/model/Sale.java
+++ retailstore/model/Sale.java
@@ -1,5 +1,21 @@
 package retailstore.model;
 
 public class Sale {
+	private String dateOfSale;
+	private String timeOfSale;
+	private String saleID;
+	private Amount totalPrice;
+	private Amount totalVAT;
+			Amount runningTotal;
+	private Amount change;
+	private Amount paidAmount;
+	
+	/**
+	 * Creates a new instance
+	 */
+	
+	public Sale () {
+		
+	}
 
 }
